65e30848ca6a7f19f044730748c8f2e6b6cd993b
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-09-15 09:39:17 -0300

    fix: reject overlapping VPC address ranges

--- modules/vpc-network/main.tf
+++ modules/vpc-network/main.tf
@@ -1,6 +1,30 @@
 locals {
-  private_service_access_address       = cidrhost(var.private_service_access_cidr, 0)
-  private_service_access_prefix_length = tonumber(split("/", var.private_service_access_cidr)[1])
+  private_service_access_address       = try(cidrhost(var.private_service_access_cidr, 0), null)
+  private_service_access_prefix_length = try(tonumber(split("/", var.private_service_access_cidr)[1]), null)
+
+  subnet_range_start = try(sum([
+    for index, octet in split(".", cidrhost(var.subnet_ip_cidr_range, 0)) :
+    tonumber(octet) * [16777216, 65536, 256, 1][index]
+  ]), -1)
+  subnet_range_end = try(sum([
+    for index, octet in split(".", cidrhost(var.subnet_ip_cidr_range, -1)) :
+    tonumber(octet) * [16777216, 65536, 256, 1][index]
+  ]), -1)
+  private_service_access_range_start = try(sum([
+    for index, octet in split(".", cidrhost(var.private_service_access_cidr, 0)) :
+    tonumber(octet) * [16777216, 65536, 256, 1][index]
+  ]), -1)
+  private_service_access_range_end = try(sum([
+    for index, octet in split(".", cidrhost(var.private_service_access_cidr, -1)) :
+    tonumber(octet) * [16777216, 65536, 256, 1][index]
+  ]), -1)
+
+  network_ranges_do_not_overlap = (
+    local.subnet_range_start < 0 ||
+    local.private_service_access_range_start < 0 ||
+    local.subnet_range_end < local.private_service_access_range_start ||
+    local.private_service_access_range_end < local.subnet_range_start
+  )
 }
 
 resource "google_compute_network" "this" {
@@ -29,6 +53,13 @@ resource "google_compute_global_address" "private_service_access" {
   address       = local.private_service_access_address
   prefix_length = local.private_service_access_prefix_length
   network       = google_compute_network.this.id
+
+  lifecycle {
+    precondition {
+      condition     = local.network_ranges_do_not_overlap
+      error_message = "subnet_ip_cidr_range and private_service_access_cidr must not overlap. Choose distinct address ranges for the workload subnet and Private Service Access allocation."
+    }
+  }
 }
 
 resource "google_service_networking_connection" "private_service_access" {
